6c117a0adb1830773400fadb04af21a5a1a3e642
SERGEYBOBKOV <sergey_bobkov@inbox.ru>
2018-12-25 16:37:33 +0300

    Вывод брендов

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -12,20 +12,10 @@ class MainController extends AppController
 
   public function indexAction()
   {
-    $posts = R::findAll('test');
+    $brands = R::find('brand', 'LIMIT 3');
+
     $this->setMeta('Заголовог главной страницы', 'Описание главной страницы', 'Ключевые слова главной страницы');
 
-    $names = ['Sergey', 'Kira'];
-
-    $name = 'John';
-    $age = 31;
-
-    $cache = Cache::instance();
-    //$cache->set('test', $names);
-    //$data = $cache->get('test');
-    //$cache->delete('test');
-    //debug($data);
-
-    $this->set(compact('name', 'age', 'posts', 'names'));
+    $this->set(compact('brands'));
   }
 }
