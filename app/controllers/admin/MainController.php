6400b4be86dc54d1979433a9a8bb8cfad56827f6
SERGEYBOBKOV <sergey_bobkov@inbox.ru>
2019-03-05 15:24:56 +0300

    Вывод заказов в админке

--- app/controllers/admin/MainController.php
+++ app/controllers/admin/MainController.php
@@ -4,10 +4,19 @@
 namespace app\controllers\admin;
 
 
+use RedBeanPHP\R;
+
 class MainController extends AppController
 {
   public function indexAction()
   {
+    $countNewOrders = R::count('order', "status = '0'");
+    $countUsers = R::count('user');
+    $countProducts = R::count('product');
+    $countCategories = R::count('category');
+
+    $this->set(compact('countNewOrders', 'countCategories', 'countProducts', 'countUsers'));
+
     $this->setMeta('Панель управления');
   }
 }
--- app/views/admin/Main/index.php
+++ app/views/admin/Main/index.php
@@ -16,14 +16,14 @@
       <!-- small box -->
       <div class="small-box bg-aqua">
         <div class="inner">
-          <h3>150</h3>
+          <h3><?php echo $countNewOrders; ?></h3>
 
-          <p>New Orders</p>
+          <p>Новых заказа</p>
         </div>
         <div class="icon">
           <i class="ion ion-bag"></i>
         </div>
-        <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+        <a href="<?php echo ADMIN; ?>/order" class="small-box-footer">Все заказы <i class="fa fa-arrow-circle-right"></i></a>
       </div>
     </div>
     <!-- ./col -->
@@ -31,14 +31,14 @@
       <!-- small box -->
       <div class="small-box bg-green">
         <div class="inner">
-          <h3>53<sup style="font-size: 20px">%</sup></h3>
+          <h3><?php echo $countProducts; ?></h3>
 
-          <p>Bounce Rate</p>
+          <p>Товаров</p>
         </div>
         <div class="icon">
           <i class="ion ion-stats-bars"></i>
         </div>
-        <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+        <a href="<?php echo ADMIN; ?>/product" class="small-box-footer">Все товары <i class="fa fa-arrow-circle-right"></i></a>
       </div>
     </div>
     <!-- ./col -->
@@ -46,14 +46,14 @@
       <!-- small box -->
       <div class="small-box bg-yellow">
         <div class="inner">
-          <h3>44</h3>
+          <h3><?php echo $countUsers; ?></h3>
 
-          <p>User Registrations</p>
+          <p>Пользователей</p>
         </div>
         <div class="icon">
           <i class="ion ion-person-add"></i>
         </div>
-        <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+        <a href="<?php echo ADMIN; ?>/user" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
       </div>
     </div>
     <!-- ./col -->
@@ -61,14 +61,14 @@
       <!-- small box -->
       <div class="small-box bg-red">
         <div class="inner">
-          <h3>65</h3>
+          <h3><?php echo $countCategories; ?></h3>
 
-          <p>Unique Visitors</p>
+          <p>Категорий</p>
         </div>
         <div class="icon">
           <i class="ion ion-pie-graph"></i>
         </div>
-        <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+        <a href="<?php echo ADMIN; ?>/category" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
       </div>
     </div>
     <!-- ./col -->
